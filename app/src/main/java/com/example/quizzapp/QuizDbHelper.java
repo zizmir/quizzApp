bafb90374c36ab6d141318b94ac88e3c09eca0e8
m'bark <mbark.bouznir@gmail.com>
2019-02-19 15:59:55 +0100

    last comit

---
+++ app/src/main/java/com/example/quizzapp/QuizDbHelper.java
@@ -0,0 +1,92 @@
+package com.example.quizzapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.quizzapp.QuizContract.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class QuizDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "MyAwesomeQuiz.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private SQLiteDatabase db;
+
+    public QuizDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        this.db = db;
+
+        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
+                QuestionsTable.TABLE_NAME + " ( " +
+                QuestionsTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuestionsTable.COLUMN_QUESTION + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION1 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION2 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION3 + " TEXT, " +
+                QuestionsTable.COLUMN_ANSWER_NR + " INTEGER" +
+                ")";
+
+        db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+        fillQuestionsTable();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + QuestionsTable.TABLE_NAME);
+        onCreate(db);
+    }
+
+    private void fillQuestionsTable() {
+        Question q1 = new Question("Qui recueille en premier le jeune Bruce Wayne à la mort de ses parents ?", "Mr Alfred Pennyworth", "Dr Leslie Thompkins", "Henri Ducard, un chasseur de tête", 2);
+        addQuestion(q1);
+        Question q2 = new Question("Comment est renommée la rue du quartier de Park Row où les parents Wayne sont assassinés ?", "Crime Street", "Crime Alley", "Murder Way", 1);
+        addQuestion(q2);
+        Question q3 = new Question("Quel film Bruce Wayne et ses parents vont-ils voir le soir où ces derniers sont assassinés ?", "Mr Alfred Pennyworth", "Dr Leslie Thompkins", "Henri Ducard, un chasseur de tête", 3);
+        addQuestion(q3);
+        Question q4 = new Question("Pourquoi Bruce Wayne choisit-il la chauve souris comme emblème ?", "Parce qu'il a été mordu par une chauve-souris étant petit", "En raison de la peur que l'animal inspire", "Parce qu'il est nyctalope", 2);
+        addQuestion(q4);
+        Question q5 = new Question("Plusieurs disciples de Batman endossent le costume de Robin. L'un d'eux est tué par Le Joker. Lequel ?", "Dick Grayson", "Jason Todd", "Tim Drake", 2);
+        addQuestion(q5);
+    }
+
+    private void addQuestion(Question question) {
+        ContentValues cv = new ContentValues();
+        cv.put(QuestionsTable.COLUMN_QUESTION, question.getQuestion());
+        cv.put(QuestionsTable.COLUMN_OPTION1, question.getOption1());
+        cv.put(QuestionsTable.COLUMN_OPTION2, question.getOption2());
+        cv.put(QuestionsTable.COLUMN_OPTION3, question.getOption3());
+        cv.put(QuestionsTable.COLUMN_ANSWER_NR, question.getAnswerNr());
+        db.insert(QuestionsTable.TABLE_NAME, null, cv);
+    }
+
+    public List<Question> getAllQuestions() {
+        List<Question> questionList = new ArrayList<>();
+        db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM " + QuestionsTable.TABLE_NAME, null);
+
+        if (c.moveToFirst()) {
+            do {
+                Question question = new Question();
+                question.setQuestion(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION3)));
+                question.setAnswerNr(c.getInt(c.getColumnIndex(QuestionsTable.COLUMN_ANSWER_NR)));
+                questionList.add(question);
+            } while (c.moveToNext());
+        }
+
+        c.close();
+        return questionList;
+    }
+}
